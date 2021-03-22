0f034a2600b31f6212c10afc9dd53766b0cf303c
nik-marinenko <nik.marinenko@gmail.com>
2021-03-22 12:40:48 +0300

    Update Form1.vb

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,13 +1,25 @@
-﻿Public Class Form1
+Public Class Form1
     Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
-
+        OpenFileDialog1.ShowDialog()
     End Sub
 
     Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
-
+        AxAcroPDF1.gotoPreviousPage()
     End Sub
 
     Private Sub AxAcroPDF1_Enter(sender As Object, e As EventArgs) Handles AxAcroPDF1.Enter
 
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        AxAcroPDF1.LoadFile(OpenFileDialog1.FileName.ToString)
+    End Sub
+
+    Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
+        AxAcroPDF1.gotoNextPage()
+    End Sub
+
+    Private Sub ToolStripButton4_Click(sender As Object, e As EventArgs) Handles ToolStripButton4.Click
+        AxAcroPDF1.Print()
     End Sub
 End Class
